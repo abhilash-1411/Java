978be9ba6cbaa124fb76640691ec72ae74add2f5
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-08 12:16:11 +0530

    data types

--- Hello.java
+++ Hello.java
@@ -5,6 +5,29 @@
         int num2=10;    
         int sum = num1 + num2;
         System.out.println("Sum: " + sum);
+
+        //Data types
+        byte b = 100; // 1 byte
+        short s = 10000; // 2 bytes         
+        int i = 100000; // 4 bytes
+        long l = 100000L; // 8 bytes
+
+        float f = 10.5f; // 4 bytes
+        double d = 20.5; // 8 bytes             
+
+        char c = 'A'; // 2 bytes
+        boolean isTrue = true; // 1 bit     
+
+        // Print data types
+        System.out.println("Byte: " + b);   
+        System.out.println("Short: " + s);
+        System.out.println("Integer: " + i);    
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + isTrue);
+        
        
     }
     
