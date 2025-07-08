2de295ad2d8e28ce28b7982a4ad01c006cf93c44
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-08 12:03:09 +0530

    variables in Java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
  class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num1=5;
+        int num2=10;    
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+       
     }
     
 }
