4e1a2e28d3fa89ade8719e4aed7117ed75104275
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-08 18:54:27 +0530

    loops in Java

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class loops{
+    public static void main(String[] args) {
+        // For loop example
+        for (int i = 0; i < 5; i++) {
+            System.out.println("For loop iteration: " + i);
+        }
+
+        // While loop example
+        int j = 0;
+        while (j < 5) {
+            System.out.println("While loop iteration: " + j);
+            j++;
+        }
+
+        // Do-while loop example
+        int k = 0;
+        do {
+            System.out.println("Do-while loop iteration: " + k);
+            k++;
+        } while (k < 5);
+    }
+}
