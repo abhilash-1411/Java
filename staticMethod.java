45bc0c3ae4737340e2405f481dbb1e88f4c93170
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-09 19:55:33 +0530

    Static method

---
+++ staticMethod.java
@@ -0,0 +1,38 @@
+class Mobile {
+    String brand;
+    int price;
+    static String model;
+
+    public void show() {
+        System.out.println("Brand: " + brand + ", Model: " + model + ", Price: " + price);
+    }
+    public static void show1(Mobile obj) {
+        System.out.println("In static method");
+        System.out.println("Model: " + model);
+        System.out.println("Model: " + model + ", Brand: " + obj.brand + ", Price: " + obj.price);
+        // we can access static variable in static method without creating an instance of the class
+    }
+}
+
+public class staticMethod {
+    public static void main(String[] args) {
+
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Samsung";
+        Mobile.model = "Galaxy S21";
+        obj1.price = 799;
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Apple";
+        Mobile.model = "iPhone 13";
+        obj2.price = 999;
+
+        Mobile.model = "Galaxy S21 Ultra"; // Static variable shared by all instances
+        obj1.show();
+        obj2.show();
+
+         Mobile.show1(obj1); // Calling static method without creating an instance
+   
+    }
+   }
+
