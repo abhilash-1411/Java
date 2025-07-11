5506faa0ecd85bb69c63b9d94d37a5bb69b50169
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-11 16:52:24 +0530

    single and multilevel  inheritance

--- inheritance.java
+++ inheritance.java
@@ -21,17 +21,25 @@ class AdvancedCalculator extends Calculator {
             throw new ArithmeticException("Division by zero is not allowed.");
         }
     }
-}    
+}   
+class veryAdvancedCalculator extends AdvancedCalculator {
+    // This class can have additional methods or override existing ones
+    public double power(int base, int exponent) {
+        return Math.pow(base, exponent);
+    }
+}
 
 public class inheritance {
     public static void main(String[] args) {
         // Calculator calc = new Calculator();
         // System.out.println("Addition: " + calc.add(5, 3));
         // System.out.println("Subtraction: " + calc.subtract(5, 3));
-        AdvancedCalculator advCalc = new AdvancedCalculator();
-        System.out.println("Addition: " + advCalc.add(5, 3));
-        System.out.println("Subtraction: " + advCalc.subtract(5, 3));
-        System.out.println("Multiplication: " + advCalc.multiply(5, 3));
-        System.out.println("Division: " + advCalc.divide(5, 0));
+        // AdvancedCalculator advCalc = new AdvancedCalculator();
+        veryAdvancedCalculator veryAdvCalc = new veryAdvancedCalculator();
+        System.out.println("Addition: " + veryAdvCalc.add(5, 3));
+        System.out.println("Subtraction: " + veryAdvCalc.subtract(5, 3));
+        System.out.println("Multiplication: " + veryAdvCalc.multiply(5, 3));
+        System.out.println("Division: " + veryAdvCalc.divide(5, 1));
+        System.out.println("Power: " + veryAdvCalc.power(2, 3));
     }
 }
