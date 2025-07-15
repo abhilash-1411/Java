cfe913131020f2e123a044dc19eb44e7f5bb12b3
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-15 16:17:40 +0530

    Lambda expression with return

--- lambdaExpre.java
+++ lambdaExpre.java
@@ -1,11 +1,15 @@
 interface A{
-    void show(int i);
+    // void show(int i);
+    int add(int a, int b);
 }
 
 
 public class lambdaExpre {
     public static void main(String[] args) {
-       A obj = ( i) -> System.out.println("This is a lambda expression in action: " + i);
-       obj.show(5);
+    //    A obj = ( i) -> System.out.println("This is a lambda expression in action: " + i);
+    //    obj.show(5);
+        A obj = (a, b) -> a + b; // Lambda expression for the add method
+        int result = obj.add(5, 10); // Calling the add method
+        System.out.println("Result of addition: " + result); // Output the result
     }
 }
