d80f8f5c0362d738ee3ad9222752acd5fd8e7e9f
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-17 12:07:06 +0530

    Stream API

--- streamAPI.java
+++ streamAPI.java
@@ -6,12 +6,27 @@ import java.util.stream.Stream;
 public class streamAPI {
     public static void main(String args[]){
         System.out.println("Hello Stram API");
-        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
-        Stream<Integer> s1=nums.stream();
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5,6);
+        // Stream<Integer> s1=nums.stream();
+        // Stream<Integer> s2=s1.filter(n->n%2==0);
+        // Stream<Integer> s3= s2.map(x->x*2);
+
+        // int result=s3.reduce(0,(c,e)->c+e);
+
+        int result=nums.stream()
+                            .filter(n->n%2==0)
+                            .map(n->(n*2))
+                            .reduce(0,(c,e)->c+e);
+              
 
         // nums.forEach((x->System.out.println(x)));
         // Consumer<Integer> con=(n)->System.out.println(n);  
-        nums.forEach((n)->System.out.println(n));   
+        // nums.forEach((n)->System.out.println(n));   
+
+        // s1.forEach((x->System.out.println(x)));
+        // s2.forEach((x->System.out.println(x)));
+        // s3.forEach((x->System.out.println(x)));
+          System.out.println(result);
 
     }
 }
